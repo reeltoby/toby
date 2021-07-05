0265ac380c46acbcc921de99e75d3cb37b6f2b55
reeltoby <reeltoby@gmail.com>
2021-07-04 21:38:48 -0400

    master

--- unzip.ps1
+++
@@ -1,3 +0,0 @@
-$backup= 7z e C:\CollaborativeProject\SCRIBE.NWCSTG.05.08.14Z.n.Z  -oC:\CollaborativeProject\output 
-$backup
-Rename-Item -Path "C:\CollaborativeProject\output\SCRIBE.NWCSTG.05.08.14Z.n" -NewName "SCRIBE.NWCSTG.05.08.14Z.txt"
